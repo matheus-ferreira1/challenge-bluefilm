5c378186d693760e10753b10f63820c76186781f
matheus-ferreira1 <matheus.fer33@gmail.com>
2023-01-25 20:57:03 -0300

    added a initial types file and fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,59 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
+import { BannerMovieTypes } from "./utils/types";
+
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import MovieDetail from "./pages/MovieDetail";
+import ActorDetail from "./pages/ActorDetail";
+
+import Header from "./components/Header";
 
 function App() {
-  return <h1>teste</h1>;
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [bannerMovies, setBannerMovies] = useState<BannerMovieTypes[]>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        setLoading(true);
+
+        const { data } = await axios.get(
+          `https://api.themoviedb.org/3/trending/movie/week?api_key=${
+            import.meta.env.VITE_TMDB_API_KEY
+          }`
+        );
+
+        const { results } = data;
+        setBannerMovies(results.slice(0, 10));
+      } catch (error) {
+        // const err = error as AxiosError;
+        // console.log(err.response?.data);
+        // setError(err.response?.data);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/movie-detail/:id" element={<MovieDetail />} />
+          <Route path="/actor-detail/:id" element={<ActorDetail />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/utils/types.ts
@@ -0,0 +1,8 @@
+export interface BannerMovieTypes {
+  id: number;
+  title: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  backdrop_path: string;
+}
