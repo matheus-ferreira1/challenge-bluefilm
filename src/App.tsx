efa5679b18cfef18076429fd91b65f512e42b933
matheus-ferreira1 <matheus.fer33@gmail.com>
2023-01-25 22:53:52 -0300

    logic for fetching data for the home page now resides inside de home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import axios, { AxiosError } from "axios";
+import { AxiosError } from "axios";
 import { useEffect, useState } from "react";
-
-import { ActorTypes, BannerMovieTypes, MovieDataTypes } from "./utils/types";
-import * as Api from "./utils/api";
 
 import Home from "./pages/Home";
 import Search from "./pages/Search";
@@ -14,37 +11,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<AxiosError | null | unknown>(null);
-  const [bannerMovies, setBannerMovies] = useState<BannerMovieTypes[]>([]);
-  const [popularMovies, setPopularMovies] = useState<MovieDataTypes[]>([]);
-  const [comingSoon, setComingSoon] = useState<MovieDataTypes[]>([]);
-  const [popularActors, setPopularActors] = useState<ActorTypes[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-
-      try {
-        const trendingMovies = await Api.getTrendingMovies();
-        setBannerMovies(trendingMovies);
-        const popularMovies = await Api.getPopularMovies();
-        setPopularMovies(popularMovies);
-        const comingSoonMovies = await Api.getComingSoonMovies();
-        setComingSoon(comingSoonMovies);
-        const popularActors = await Api.getPopularActors();
-        setPopularActors(popularActors);
-      } catch (error: unknown | AxiosError) {
-        console.log(error);
-        setError(error);
-        setLoading(false);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
   return (
     <div>
       <Router>
@@ -52,7 +18,7 @@ function App() {
 
         <Routes>
           <Route path="/home" element={<Home />} />
-          <Route path="/search" element={<Search />} />
+          <Route path="/search/:term" element={<Search />} />
           <Route path="/movie-detail/:id" element={<MovieDetail />} />
           <Route path="/actor-detail/:id" element={<ActorDetail />} />
         </Routes>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,42 @@
-import React from "react";
+import { FC, useEffect, useState } from "react";
+import { AxiosError } from "axios";
+import { ActorTypes, BannerMovieTypes, MovieDataTypes } from "../utils/types";
+import * as Api from "../utils/api";
 
-const Home = () => {
+const Home: FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<AxiosError | null | unknown>(null);
+  const [bannerMovies, setBannerMovies] = useState<BannerMovieTypes[]>([]);
+  const [popularMovies, setPopularMovies] = useState<MovieDataTypes[]>([]);
+  const [comingSoon, setComingSoon] = useState<MovieDataTypes[]>([]);
+  const [popularActors, setPopularActors] = useState<ActorTypes[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const trendingMovies = await Api.getTrendingMovies();
+        setBannerMovies(trendingMovies);
+        console.log(trendingMovies);
+
+        const popularMovies = await Api.getPopularMovies();
+        setPopularMovies(popularMovies);
+        const comingSoonMovies = await Api.getComingSoonMovies();
+        setComingSoon(comingSoonMovies);
+        const popularActors = await Api.getPopularActors();
+        setPopularActors(popularActors);
+      } catch (error: unknown | AxiosError) {
+        console.log(error);
+        setError(error);
+        setLoading(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return <div>Home</div>;
 };
 
